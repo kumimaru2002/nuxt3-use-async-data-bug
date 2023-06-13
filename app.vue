3e5c5f5e926f5fe404b06bc0f3a86ee412d45b19
kumimaru <blankr.kumi1@gmail.com>
2023-06-13 14:51:23 +0900

    🚀error repository

---
+++ app.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+// -----------------------------------
+// test case 1: bug
+// -----------------------------------
+const { data } = await useAsyncData(async () => {
+  const coreObject = ref({
+    nestText: 'success',
+  });
+  const wrappar = ref({
+    coreObject,
+  });
+
+  return {
+    wrappar,
+  };
+});
+if (!data.value) {
+  throw new Error('data is undefined');
+}
+// value: success
+// console error: TypeError: Cannot read properties of undefined (reading 'coreObject')
+console.log(data.value.wrappar.coreObject.nestText);
+
+// value: undefined
+// no console error
+console.log(data.value.wrappar.value.coreObject.nestText);
+3;
+// -----------------------------------
+// test case 2: success
+// -----------------------------------
+
+const coreObject = ref({
+  nestText: 'success',
+});
+const wrappar = ref({
+  coreObject,
+});
+
+// value: success
+console.log(wrappar.value.coreObject.nestText);
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
